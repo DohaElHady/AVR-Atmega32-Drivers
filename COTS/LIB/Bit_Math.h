023af85707637c826ab699c501077b174198e550
Doha ElHady <86476979+DohaElHady@users.noreply.github.com>
2022-01-07 19:36:03 +0200

    Update Bit_Math.h

--- COTS/LIB/Bit_Math.h
+++ COTS/LIB/Bit_Math.h
@@ -9,10 +9,10 @@
 #define  CIR_SHIFT_R_BIT(REG)             REG  =   ((GET_BIT(REG,0) << 7) | (REG>>1))
 #define  SET_NIBBLE(REG,CASE)             REG |=   (0x0F<<(4*CASE))
 #define  CLR_NIBBLE(REG,CASE)             REG &=   (0xF0>>(4*CASE))
-#define  ASG_NIBBLE(REG,CASE,VALUE)		    REG  =	 (CASE==0?((REG&0xF0)|(VALUE&0x0F)):((REG&0x0F)|(VALUE&0xF0)))
+#define  ASG_NIBBLE(REG,CASE,VALUE)       REG  =	 (CASE==0?((REG&0xF0)|(VALUE&0x0F)):((REG&0x0F)|(VALUE&0xF0)))
 #define  SET_PORT(REG)                    REG  =   (0xFF)
 #define  CLR_PORT(REG)                    REG  =   (0x00)
-#define  ASG_PORT(REG,VALUE)				      REG	 =	 (VALUE)
+#define  ASG_PORT(REG,VALUE)              REG	 =	 (VALUE)
 
 
 #endif
