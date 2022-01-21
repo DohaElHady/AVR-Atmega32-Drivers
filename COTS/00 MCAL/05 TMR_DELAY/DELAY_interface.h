700c1c4a188d4fc247369daa4bded3c72fd16323
Doha ElHady <86476979+DohaElHady@users.noreply.github.com>
2022-01-21 22:50:00 +0200

    Delete COTS/00 MCAL/05 TMR_DELAY directory

--- COTS/00 MCAL/05 TMR_DELAY/DELAY_interface.h
+++
@@ -1,7 +0,0 @@
-#ifndef		DELAY_INTERFACE_H
-#define		DELAY_INTERFACE_H
-
-void	    DELAY_TMROVFISR(void);
-void      DELAY_voidDelayMS(u8 DelayMS);
-void      DELAY_voidDelayUS(u8 DelayUS);
-#endif
--- COTS/00 MCAL/05 TMR_DELAY/DELAY_private.h
+++
@@ -1,8 +0,0 @@
-#ifndef		DELAY_PRIVATE_H
-#define		DELAY_PRIVATE_H
-
-#define   TMR0_DELAY       0
-#define   TMR2_DELAY       2
-
-
-#endif
